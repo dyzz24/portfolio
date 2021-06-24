7a04fa0ed022519423db202e49157c1ac8a2ae53
Васильков Дмитрий Михайлович <DVasilkov@stream.ru>
2021-06-24 18:10:33 +0300

    polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
+import 'react-app-polyfill/stable';
+import 'core-js/features/array/from';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
