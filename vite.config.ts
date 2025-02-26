7224f7b34bcad19a4d3d3d9578e029a6608f60ba
dvasilkov <dvasilkov@accesssoftek.com>
2025-02-26 16:20:55 +0300

    build fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    modules: {
-      scopeBehaviour: 'local'
+    plugins: [react()],
+    css: {
+        modules: {
+            scopeBehaviour: 'local',
+        },
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+            },
+        },
     },
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-      }
-    }
-  }
+    build: {
+        outDir: 'build',
+    },
 })
